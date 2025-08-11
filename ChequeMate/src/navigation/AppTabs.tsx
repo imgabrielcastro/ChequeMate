2a28915c54bf78891c34f93ec3e761a5f6034c3d
Gabriel Souza de Castro <110572481+imgabrielcastro@users.noreply.github.com>
2025-08-11 11:57:07 -0300

    fix button hover android

--- ChequeMate/src/navigation/AppTabs.tsx
+++ ChequeMate/src/navigation/AppTabs.tsx
@@ -1,8 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import { theme } from "../../src/themes/theme";
-import { TouchableOpacity } from "react-native";
-
+import { TouchableOpacity, Platform } from "react-native";
+import type { BottomTabBarButtonProps } from "@react-navigation/bottom-tabs";
 
 export type AppTabsParamList = {
   Início: undefined;
@@ -12,41 +12,63 @@ export type AppTabsParamList = {
 
 const Tab = createBottomTabNavigator<AppTabsParamList>();
 
+const CustomTabBarButton = (props: BottomTabBarButtonProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      onPress={props.onPress}
+      style={props.style}>
+      {props.children}
+    </TouchableOpacity>
+  );
+};
+
 export function AppTabs() {
   return (
-<Tab.Navigator
-  screenOptions={{
-    headerShown: false,
-    tabBarStyle: {
-      backgroundColor: theme.colors.input,
-      paddingHorizontal: 10,
-      height: 110,
-      borderTopWidth: 0, 
-      
-    },
-    tabBarLabelStyle: {
-      fontWeight: 'bold',
-      fontSize: 18,
-    },
-    tabBarActiveTintColor: theme.colors.primary,
-    tabBarInactiveTintColor: theme.colors.text,
-  }}
->
-  <Tab.Screen
-    name="Início"
-    component={HomeScreen}
-    options={{ tabBarLabel: "Início", tabBarIcon: () => null }}
-  />
-  <Tab.Screen
-    name="Clientes"
-    component={HomeScreen}
-    options={{ tabBarLabel: "Clientes", tabBarIcon: () => null }}
-  />
-  <Tab.Screen
-    name="Cheques"
-    component={HomeScreen}
-    options={{ tabBarLabel: "Cheques", tabBarIcon: () => null }}
-  />
-</Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: theme.colors.input,
+          paddingHorizontal: 10,
+          height: 110,
+          borderTopWidth: 0,
+        },
+        tabBarLabelStyle: {
+          fontWeight: "bold",
+          fontSize: 18,
+        },
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.text,
+      }}
+    >
+      <Tab.Screen
+        name="Início"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Início",
+          tabBarIcon: () => null,
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Clientes"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Clientes",
+          tabBarIcon: () => null,
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+      />
+      <Tab.Screen
+        name="Cheques"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Cheques",
+          tabBarIcon: () => null,
+          tabBarButton: (props) => <CustomTabBarButton {...props} />,
+        }}
+      />
+    </Tab.Navigator>
   );
 }
